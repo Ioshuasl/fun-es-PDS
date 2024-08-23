d18d1414571a4d86493e27a5b25ecfcff61075da
Ioshua Souza <souzaioshua2234@gmail.com>
2024-08-23 16:25:13 -0300

    Delete testes/testes directory

--- testes/testes/app.js
+++
@@ -1,5 +0,0 @@
-import { Aluno } from "./aluno.js";
-
-let aluno = new Aluno('ioshua','11,02,2005','ioshua@email.com')
-console.log(aluno.adiconarTreino('peito','2024,09,5','09:00','10:00',10,'supino...'))
-console.log(aluno.treinos.length)
