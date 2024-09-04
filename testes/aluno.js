657930964b9c2a4fc89d5fe3eb1c25862e70bce4
Ioshua Souza <souzaioshua2234@gmail.com>
2024-09-04 14:37:25 -0300

    NOVA FUNCAO PARA BUSCAR CLIENTES DO DIA ATUAL
    criado a função alunosComTreinoNoDia() para buscar e filtrar alunos que tem treinos no dia específico, vai ser importante para puxar os clientes do dia atual, e quando cliente quiser filtrar um dia específico na agenda

--- testes/aluno.js
+++ testes/aluno.js
@@ -6,12 +6,14 @@ export class Aluno {
         this.dataNasc = dataNasc
         this.email = email
         this.treinos = []
+        this.grupoTreino = []
     }
 
     adicionarTreino(titulo,dataInicial,horaInicio,horaTermnino,qtdTreinos, descricao){
         const treino = new Treino(titulo,dataInicial,horaInicio,horaTermnino,qtdTreinos, descricao)
         const sessaotreinos = treino.chamarEvento(titulo,dataInicial,horaInicio,horaTermnino,qtdTreinos, descricao)
         this.treinos = this.treinos.concat(sessaotreinos)
+        this.grupoTreino.push(sessaotreinos)
     }
 
     getTreino(){
@@ -46,3 +48,16 @@ export class Aluno {
         return this.treinos.filter(treino => treino.titulo === titulo)
     }
 }
+
+// let ioshua = new Aluno('ioshua','11/02/2005','ioshua@email.com')
+// ioshua.adicionarTreino('peito','2024,09,5','09:00','10:00',10,'supino...')
+// let data = new Date('2024,9,5')
+// console.log(ioshua.treinos.some(treino => treino.dataInicial.getTime() === data))
+// console.log(ioshua.treinos[0].dataInicial.getTime())
+// console.log(data)
+
+// if (ioshua.treinos[0].dataInicial.getTime() == data.getTime()){
+//     console.log('true')
+// } else {
+//     console.log('false')
+// }
--- testes/galunos.js
+++ testes/galunos.js
@@ -21,7 +21,28 @@ export class GerenciadorAlunos{
         })
     }
 
+    getTreinosDeAlunos(index, indexTreino){
+        console.log(this.alunos[index].nome)
+        console.log(this.alunos[index].treinos[indexTreino])
+    }
+
     encontrarAluno(nome){
         return this.alunos.findIndex(nome => nome === nome)
+    }
+
+    getCompromissoHoje(dataAtual) {
+        let data = new Date(dataAtual)
+        let alunosNoDia = [];
+
+        this.alunos.forEach(aluno => {
+            const temTreinoNoDia = aluno.treinos.findIndex(treino => treino.dataInicial.getTime() === data.getTime());
+            const objetoAluno = {
+                nome: aluno.nome,
+                treino: aluno.treinos[temTreinoNoDia]
+            }
+            alunosNoDia.push(objetoAluno)
+        });
+
+        return alunosNoDia;
     }
 }
