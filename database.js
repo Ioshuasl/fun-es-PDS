44f56f92af8e0477d72fcbcd69d33f88fe0c6738
Ioshua Souza <souzaioshua2234@gmail.com>
2024-09-20 11:43:47 -0300

    Delete database.js

--- database.js
+++
@@ -1,9 +0,0 @@
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize('postgres', 'postgres', '1234', {
-  host: 'localhost',
-  dialect: 'postgres',
-  port: 5432
-});
-
-export default sequelize;
